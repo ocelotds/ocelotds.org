76d4eb8fb63473f9ff487b1315a6916d53de9a2d
hhfrancois <francois.achache@gmail.com>
2015-11-22 17:23:48 +0100

    separate producer and services improve doc

--- ocelotds.ejb/src/main/java/org/ocelotds/charts/ChartServices.java
+++ ocelotds.ejb/src/main/java/org/ocelotds/charts/ChartServices.java
@@ -6,38 +6,19 @@ package org.ocelotds.charts;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Random;
-import javax.ejb.Schedule;
-import javax.ejb.Singleton;
-import javax.ejb.Startup;
-import javax.enterprise.event.Event;
-import javax.inject.Inject;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
 import org.ocelotds.Constants;
 import org.ocelotds.annotations.DataService;
-import org.ocelotds.annotations.JsTopicEvent;
-import org.ocelotds.annotations.TransientDataService;
-import org.ocelotds.marshalling.annotations.JsonMarshaller;
 
 /**
  *
  * @author hhfrancois
  */
-@Singleton
-@Startup
 @DataService(resolver = Constants.Resolver.EJB)
+@Stateless
+@LocalBean
 public class ChartServices {
-
-	@Inject
-	@JsTopicEvent("values")
-	@JsonMarshaller(DataMarshaller.class)
-	Event<Object> topicEvent;
-
-	@Schedule(dayOfWeek = "*", month = "*", hour = "*", dayOfMonth = "*", year = "*", minute = "*", second = "*/1", persistent = false)
-	@TransientDataService
-	public void getNewData() {
-		Random randomGenerator = new Random();
-		topicEvent.fire(new Object[]{new Date(), randomGenerator.nextFloat()});
-	}
 
 	public List<Object[]> getDatas(int nb) {
 		List<Object[]> datas = new ArrayList<>();
---
+++ ocelotds.ejb/src/main/java/org/ocelotds/charts/DataPublisher.java
@@ -0,0 +1,35 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package org.ocelotds.charts;
+
+import java.util.Date;
+import java.util.Random;
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.enterprise.event.Event;
+import javax.inject.Inject;
+import org.ocelotds.annotations.JsTopicEvent;
+import org.ocelotds.annotations.TransientDataService;
+import org.ocelotds.marshalling.annotations.JsonMarshaller;
+
+/**
+ *
+ * @author hhfrancois
+ */
+@Singleton
+@Startup
+public class DataPublisher {
+
+	@Inject
+	@JsTopicEvent("values")
+	@JsonMarshaller(DataMarshaller.class)
+	Event<Object> topicEvent;
+
+	@Schedule(dayOfWeek = "*", month = "*", hour = "*", dayOfMonth = "*", year = "*", minute = "*", second = "*/1", persistent = false)
+	public void publishDatas() {
+		Random randomGenerator = new Random();
+		topicEvent.fire(new Object[]{new Date(), randomGenerator.nextFloat()});
+	}
+}
