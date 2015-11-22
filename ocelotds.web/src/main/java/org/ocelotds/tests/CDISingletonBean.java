ec35645047bcc93e9517e6b63bf0b2bf0a1165a6
hhfrancois <francois.achache@gmail.com>
2015-11-22 03:02:01 +0100

    create test CDISingletonBean

---
+++ ocelotds.web/src/main/java/org/ocelotds/tests/CDISingletonBean.java
@@ -0,0 +1,23 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package org.ocelotds.tests;
+
+import java.io.Serializable;
+import javax.enterprise.context.ApplicationScoped;
+import org.ocelotds.Constants;
+import org.ocelotds.annotations.DataService;
+
+/**
+ *
+ * @author hhfrancois
+ */
+@DataService(resolver = Constants.Resolver.CDI)
+@ApplicationScoped
+public class CDISingletonBean implements Serializable {
+	int value = 0;
+
+	public int getValue() {
+		return value++;
+	}
+}
