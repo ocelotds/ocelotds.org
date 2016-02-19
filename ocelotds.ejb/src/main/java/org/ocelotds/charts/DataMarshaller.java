9717283c41314286aed93b7f7db2cf6a7553ea2b
hhfrancois <francois.achache@gmail.com>
2016-02-19 02:16:27 +0100

    create templater for Constant or factory. merge JsonMarshaller and JsonUnmarshaller interface to IJsonMArshaller with 2 methods.

--- ocelotds.ejb/src/main/java/org/ocelotds/charts/DataMarshaller.java
+++ ocelotds.ejb/src/main/java/org/ocelotds/charts/DataMarshaller.java
@@ -4,19 +4,25 @@
 package org.ocelotds.charts;
 
 import java.util.Date;
-import org.ocelotds.marshalling.JsonMarshaller;
+import org.ocelotds.marshalling.IJsonMarshaller;
 import org.ocelotds.marshalling.exceptions.JsonMarshallingException;
+import org.ocelotds.marshalling.exceptions.JsonUnmarshallingException;
 
 /**
  * 
  * @author hhfrancois
  */
-public class DataMarshaller implements JsonMarshaller<Object>{
+public class DataMarshaller implements IJsonMarshaller<Object>{
 	@Override
 	public String toJson(Object obj) throws JsonMarshallingException {
 		Object[] datas = (Object[]) obj;
 		Date d = (Date) datas[0];
 		float f = (float) datas[1];
 			return String.format("[%s,%s]", d.getTime(), f);
+	}
+
+	@Override
+	public Object toJava(String json) throws JsonUnmarshallingException {
+		return null;
 	}
 }
