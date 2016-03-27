115a458b9d9c8183e5714ab818e6bdb934350263
hhfrancois <francois.achache@gmail.com>
2016-03-27 22:19:27 +0200

    remove addOpenListener + addCloseListener

--- ocelotds.web/src/main/webapp/demo/charts/charts.js
+++ ocelotds.web/src/main/webapp/demo/charts/charts.js
@@ -1,36 +1,31 @@
 'use strict';
 if(window.chartServices === undefined) alert("Thid demo works only in Java EE server");
-ocelotController.addOpenListener(function () {
-   Highcharts.setOptions({
-      global: {useUTC: false}
-   });
-   var chart;
-   chartServices.getDatas(1000).then(function(datas) {
-      chart = new Highcharts.StockChart({
-         chart: {renderTo: 'graph-area'},
-         rangeSelector: {
-            buttons: [
-               {count: 30, type: 'second', text: '30s'}, 
-               {count: 2, type: 'minute', text: '2m'},
-               {count: 5, type: 'minute', text: '5m'}, 
-               {type: 'all', text: 'All'}
-            ],
-            inputEnabled: false,
-            selected: 0
-         },
-         title: {text: 'Live random data'},
-         exporting: {enabled: true},
-         series: [{data: datas}]
-      });
-   });
-   new Subscriber("values").then(function(nb) {
-		console.log("Subscribers to values :"+nb);
-	}).catch(function(fault) {
-		console.log(fault);
-	}).message(function (vals) {
-      chart.series[0].addPoint([vals[0], vals[1]], true, true);
-   });
+Highcharts.setOptions({
+	global: {useUTC: false}
 });
-ocelotController.addCloseListener(function () {
-   ocelotController.open();
+var chart;
+chartServices.getDatas(1000).then(function(datas) {
+	chart = new Highcharts.StockChart({
+		chart: {renderTo: 'graph-area'},
+		rangeSelector: {
+			buttons: [
+				{count: 30, type: 'second', text: '30s'}, 
+				{count: 2, type: 'minute', text: '2m'},
+				{count: 5, type: 'minute', text: '5m'}, 
+				{type: 'all', text: 'All'}
+			],
+			inputEnabled: false,
+			selected: 0
+		},
+		title: {text: 'Live random data'},
+		exporting: {enabled: true},
+		series: [{data: datas}]
+	});
+});
+new Subscriber("values").then(function(nb) {
+	console.log("Subscribers to values :"+nb);
+}).catch(function(fault) {
+	console.log(fault);
+}).message(function (vals) {
+	chart.series[0].addPoint([vals[0], vals[1]], true, true);
 });
